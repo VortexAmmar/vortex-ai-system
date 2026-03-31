c4d96b09888702ec3126a6c721a6d27c487665f5
VortexAmmar <ammar559377@gmail.com>
2026-03-31 09:45:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
 @app.route("/")
 def home():
-  return "OK" 
+    return "OK"
+
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=8080)  
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080))
